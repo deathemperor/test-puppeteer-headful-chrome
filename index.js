119b27445eeb3e3cf675c90bf611f243ec082015
deathemperor <deathemperor@gmail.com>
2018-12-14 10:52:40 +0700

    switch back to use chrome because the bundled chrome executable is chromium which does not have mp4 codec

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 const puppeteer = require('puppeteer')
 const glob = require('glob')
 let chromePath = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
-const numberOfSpams = 10
+const numberOfSpams = 1
 const urlToSpam = 'https://blog.uiza.io/spotify-squad-framework-part-i/'
 let lookFor
 
 
 var osvar = process.platform
 if (osvar == 'darwin') {
-  lookFor = "**/Chromium" // MacOSX
- }else if(osvar == 'win32'){
-  lookFor = "**/chrome.exe" // Windows
-}else{
+  // lookFor = "**/Chromium" // MacOSX
+} else if(osvar == 'win32'){
+  chromePath = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+} else {
   console.log("unknown os")
   process.exit()
 }
 
-const files = glob.sync(__dirname + `/node_modules/puppeteer/.local-chromium/${lookFor}`, {})
-chromePath = files[0] // assuming only fond out...
+// const files = glob.sync(__dirname + `/node_modules/puppeteer/.local-chromium/${lookFor}`, {})
+// chromePath = files[0] // assuming only fond out...
 
 void (async () => {
   const browser = await puppeteer.launch({ 
     headless: false, 
     executablePath: chromePath 
   })
-  for (var i = 0; i <= numberOfSpams; i++) {
+  for (var i = 1; i <= numberOfSpams; i++) {
     try {
       var page = await browser.newPage()
       await page.goto(urlToSpam)
-      page.evaluate(_ => {
+      await page.evaluate(_ => {
         window.scrollBy(0, 500)
+        // window.setTimeout(() => {
+        //   if (window.UZ.loaded) {
+        //     window.UZ.Player("iframe-2c08f440-a1d7-4a2b-941f-7fdadd5ac112").play()
+        //   }
+        // }, 2000)
       })
       await page.waitFor(2000)
     } catch (err) {
