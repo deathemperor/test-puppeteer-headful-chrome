4776cd10c662a2ef66062258987011258181db96
deathemperor <deathemperor@gmail.com>
2019-01-10 16:49:01 +0700

    better handling of exceptions + show success count of successful opened pages

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const puppeteer = require('puppeteer')
-const glob = require('glob')
 const randomUserAgent = require('random-useragent')
 const fs = require('fs');
 
 let chromePath = '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
-const numberOfSpams = 2
+const numberOfSpams = 5
 const useRandomUserAgent = true
 const openNewBrowser = true
 const urlToSpam = 'https://blog.uiza.io/spotify-squad-framework-part-i/'
-let lookFor
-
 
 var osvar = process.platform
 if (osvar == 'darwin') {
-  // lookFor = "**/Chromium" // MacOSX
-} else if(osvar == 'win32'){
+  // MacOSX
+} else if (osvar == 'win32') {
   chromePath = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
 } else {
   console.log("unknown os")
@@ -23,40 +20,55 @@ if (osvar == 'darwin') {
 
 // it is required to have Chrome to run
 if (!fs.existsSync(chromePath)) {
-  console.log('\x1b[31m', "Chrome (not Chromium) is required. Please install it here: https://chrome.google.com", '\x1b[0m' );
+  console.log('\x1b[31m', "Chrome (not Chromium) is required. Please install it here: https://chrome.google.com", '\x1b[0m');
   process.exit()
 }
 
-// const files = glob.sync(__dirname + `/node_modules/puppeteer/.local-chromium/${lookFor}`, {})
-// chromePath = files[0] // assuming only fond out...
-
-void (async () => {
+void(async () => {
   // a browser a page
+  let successCount = 0
   if (openNewBrowser) {
     for (var i = 1; i <= numberOfSpams; i++) {
       try {
-        var browser = await puppeteer.launch({ 
-          headless: false, 
-          executablePath: chromePath 
+        var browser = await puppeteer.launch({
+          headless: false,
+          executablePath: chromePath,
+          devtools: true
         })
-        var page = await browser.newPage()
+        try {
+          var page = await browser.newPage()
+        } catch (err) {
+          console.log(err)
+        }
         if (useRandomUserAgent) {
           page.setUserAgent(randomUserAgent.getRandom())
         }
-        await page.goto(urlToSpam)
-        await page.evaluate(_ => {
-          window.scrollBy(0, 500)
-        })
-        await page.waitFor(2000)
+        try {
+          await page.goto(urlToSpam)
+          // turn the below line on to enable logging chrome's console to node console
+          //page.on('console', msg => console.log('PAGE LOG:', msg.text()))
+          successCount = await page.evaluate(successCount => {
+            window.scrollBy(0, 500)
+            successCount++
+            return successCount
+          }, successCount)
+          console.log("SUCCESS COUNT: %d", successCount)
+          await page.waitFor(2000)
+        } catch (err) {
+          if (err.errno == -4048) {
+            console.log("error controlling instance")
+            browser.close()
+          }
+        }
       } catch (err) {
         console.log(err)
       }
     }
   } else {
     // multiple tabs under the same browser
-    var browser = await puppeteer.launch({ 
-      headless: false, 
-      executablePath: chromePath 
+    var browser = await puppeteer.launch({
+      headless: false,
+      executablePath: chromePath
     })
     for (var i = 1; i <= numberOfSpams; i++) {
       try {
@@ -74,5 +86,5 @@ void (async () => {
       }
     }
   }
-  
+
 })()
